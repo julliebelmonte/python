039169a5df9577a1ef4fcee62506583e65332425
julliebelmonte <jullie.bel.aguiar@gmail.com>
2022-04-20 18:41:19 -0300

    codecademy

--- lists.py
+++ lists.py
@@ -74,3 +74,66 @@ print(customer_data)
 
 customer_data_final= customer_data + [["Amit", "Large", True], ["Karim", "X-Large", False]]
 print(customer_data_final)
+
+
+last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
+subjects= ["physics", "calculus", "poetry", "history"]
+grades= [98, 97, 85, 88]
+
+gradebook= [["physics",	98], ["calculus",	97], ["poetry",	85], ["history",	88]]
+print(gradebook)
+
+gradebook.append(["computer science", 100])
+gradebook.append(["visual arts", 93])
+
+gradebook[-1][-1]= 98
+
+gradebook[2].remove(85)
+gradebook[2].append("Pass")
+print(gradebook)
+
+full_gradebook = last_semester_gradebook + gradebook
+print(full_gradebook)
+
+
+# pizza
+
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+
+prices= [ 2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices= prices.count(2)
+
+num_pizzas= len(toppings)
+
+print("We sell " + str(num_pizzas) + " different kinds of pizza!")
+
+pizza_and_prices= [[2,	"pepperoni"],[6,"pineapple"], [1,	"cheese"], [3,	"sausage"], [2,	"olives"], [7,	"anchovies"], [2,	"mushrooms"]]
+print(pizza_and_prices)
+
+pizza_and_prices.sort()
+
+print(pizza_and_prices)
+
+cheapest_pizza= pizza_and_prices[2]
+
+priciest_pizza= pizza_and_prices[5]
+
+pizza_and_prices.remove([7, "anchovies"])
+
+pizza_and_prices[5].append([2.5, "peppers"])
+
+print(pizza_and_prices)
+
+three_cheapest= pizza_and_prices[:3]
+
+print(three_cheapest)
+
+
+
+
+
+
+
+
+
